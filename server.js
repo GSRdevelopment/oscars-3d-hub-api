b840cd62ffba6c3d4b22b926ccec1440ac79b775
GSRdevelopment <gsrdevelopment.contact@gmail.com>
2020-09-05 08:21:47 -0400

    added cors dependency

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
+const cors = require('cors');
+
+app.use(cors());
 
 dotenv.config({ path: './config.env' });
 
