a71e269410ecb996e6acc93d821c29d0778c32b3
GSRdevelopment <gsrdevelopment.contact@gmail.com>
2020-09-12 13:00:39 -0400

    added cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const modelRouter = require('./routes/modelRoutes');
+const cors = require('cors');
 
 const app = express();
 
 // MIDDLEWARES
+app.use(cors());
 app.use(express.json());
 
 // ROUTES
