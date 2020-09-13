fe8aeadc9e596d32942495738feb4b876504392f
GSRdevelopment <gsrdevelopment.contact@gmail.com>
2020-09-13 11:06:35 -0400

    change bodyparser limit

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = express();
 
 // MIDDLEWARES
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
 // ROUTES
 app.use('/api/v1/models', modelRouter);
